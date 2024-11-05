14d439d922a8ced8462af2bb4675905a18cc0159
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-05 13:55:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    MONGO_URI = "mongodb://localhost:27017/language_exchange"  # Use your MongoDB URI
+    SECRET_KEY = "kdkdkdldlslhej"  # Replace with a real secret key
+    JWT_SECRET_KEY = "rjrjrjrjrkkr"  # Replace with a real JWT key
